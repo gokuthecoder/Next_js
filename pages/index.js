1062996da5be3004d7404946c23a84a4387d687f
gokuthecoder <vishalkr12012001@gmail.com>
2023-12-17 13:04:43 +0530

    layout components

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import Head from 'next/head';
+import Layout from "../../components/Layout";
 
 
 export default function FirstPost() {
     return (
         <>
-            <h1>First Post Madarchod Betichod</h1>
-            Learn <Link href="your-blog.com/posts/first-post">Next.js!</Link>
-            <Head>
-                <title>first page</title>
-            </Head>
+            <Layout>
+                <h1>First Post Madarchod Betichod</h1>
+                Learn <Link href="/">Next.js!</Link>
+                <Head>
+                    <title>first page</title>
+                </Head>
+            </Layout>
         </>
     )
 }
